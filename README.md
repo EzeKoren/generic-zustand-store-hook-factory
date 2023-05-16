a63d767463b6869ba979ebdcb576ee7b4fa99a17
Ezequiel Korenblum <ezequiel.korenblum@urbetrack.com>
2023-05-16 16:50:54 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# generic-zustand-store-hook-factory
+
+## A simple library to wrap a Zustand store in a React Hook with a simple getter and setter, complete with safe typing 
---
+++ index.ts
@@ -0,0 +1,15 @@
+import { type StoreApi, type UseBoundStore } from "zustand";
+
+export const genericStoreHookFactory = <T extends {}>(store: UseBoundStore<StoreApi<T>>) => {
+  const getStore = <R extends keyof T>(key: R): T[R] => store((state) => state[key]);
+
+  const setStore = <S extends keyof T>(key: S, value: T[S]) => {
+    const newState: Partial<T> = {};
+
+    newState[key] = value;
+
+    store.setState(newState);
+  };
+
+  return { getStore, setStore };
+};
